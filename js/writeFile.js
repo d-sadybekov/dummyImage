fad17c1176824d7e97b686bd06db563297d9841b
d.sadybekov <d.sadybekov@yandex.ru>
2023-03-20 13:06:49 +0500

    create backend service

--- js/writeFile.js
+++ js/writeFile.js
@@ -1,19 +1,47 @@
-import fs from 'node:fs'
-import { Buffer } from 'node:buffer'
-const requiredSize=1000000
-const reqBuf = (imgBuf,reqSize) =>{
-  const targetBuf= Buffer.alloc(reqSize,0)
+import fs from "node:fs"
+import { Buffer } from "node:buffer"
+import { v4 as uuidv4 } from 'uuid';
+
+const reqBuf = (imgBuf, reqSize) => {
+  const targetBuf = Buffer.alloc(reqSize, 0)
   imgBuf.copy(targetBuf)
   return targetBuf
 }
-fs.readFile('C:/Users/tech/Desktop/TEmp/min/min.bmp', (err, data) => {
-  if (err) throw err;
-  fs.writeFile('C:/Users/tech/Desktop/TEmp/resize/resize.bmp', reqBuf(data,requiredSize), (err) => {
-    if (err) throw err;
-    console.log('The file has been saved!')
-    fs.stat('new.jpg', (err, size) => {
-      if (err) throw err;
-      console.log(size)
-    })
-  })  
-})
+const srcPath = (type) => {
+  const path = "./res/min." + type
+  return path
+}
+const resultPath = (type) => {
+  const path = "./result/"+uuidv4()+"." + type
+  return path
+}
+
+const readAndGenerate = (type = "jpg", reqSize = 0) => {
+  try {
+    const link = "my_link_from_node"
+    // console.log('Перед IF',reqSize)
+    if (reqSize >= 999 && reqSize <= 9999999) {
+      fs.readFile(srcPath(type), (err, data) => {
+        if (err) throw err
+        fs.writeFile(resultPath(type), reqBuf(data, Number(reqSize)), (err) => {
+          if (err) throw err
+          console.log("The file has been saved!")
+        })
+      })
+      return {
+        fileType: type,
+        requiredSize: String(reqSize),
+        resultPath: resultPath(type),
+        downloadLink: link,
+      }
+    } else {
+      const errMes = { error: "invalid required file size" }
+
+      return errMes
+    }
+  } catch (err) {
+    console.log("rng: ", err)
+  }
+}
+
+export default readAndGenerate
